4473631ecd84900da4724ec0de93668b1ae0d95b
Verkhovtsev Leonid <leoromanovich@gmail.com>
2020-03-18 11:41:31 +0300

    redirect off

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -338,7 +338,7 @@ tls-crypt tc.key
 topology subnet
 server 10.8.0.0 255.255.255.0
 ifconfig-pool-persist ipp.txt" > /etc/openvpn/server/server.conf
-	echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server/server.conf
+	# echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server/server.conf
 	# DNS
 	case "$dns" in
 		1|"")
